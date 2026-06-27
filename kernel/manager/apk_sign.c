940aae50d70397b2519c492ca87ca4dca1cd9317
manipvlator <mr.morat14@gmail.com>
2026-06-27 14:54:54 +0700

    kernel: apk_sign: add more kernelsu fork support
    added support for
    - KernelSU-Next
    - MamboSU
    - RKSU
    - WildKSU
    - ReSukiSU
    
    Signed-off-by: manipvlator <mr.morat14@gmail.com>

--- kernel/manager/apk_sign.c
+++ kernel/manager/apk_sign.c
@@ -374,5 +374,25 @@ bool is_manager_apk(char *path)
 	if (check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588"))
 		return true;
 
-	return false;
+	// rifsxd/KernelSU-Next
+	if (check_v2_signature(path, 0x3e6, "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7"))
+		return true;
+
+	// rsuntk/KernelSU
+	if (check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b"))
+		return true;
+
+	// RapliVx/KernelSU
+	if (check_v2_signature(path, 0x384, "a9462b8b98ea1ca7901b0cbdcebfaa35f0aa95e51b01d66e6b6d2c81b97746d8"))
+		return true;
+
+	// WildKernels/Wild_KSU
+	if (check_v2_signature(path, 0x381, "52d52d8c8bfbe53dc2b6ff1c613184e2c03013e090fe8905d8e3d5dc2658c2e4"))
+		return true;
+
+	// ReSukiSU/ReSukiSU
+    if (check_v2_signature(path, 0x377, "d3469712b6214462764a1d8d3e5cbe1d6819a0b629791b9f4101867821f1df64"))
+        return true;
+
+        return false;
 }
